2a90eb0a6e33ee80673f7b69109d5da6de31ea22
Michael <support@aussieserverhosts.com>
2021-05-03 21:12:05 +1000

    Fix to read variables

--- arkmods.sh
+++ arkmods.sh
@@ -25,16 +25,23 @@ sed -i '/^$/d' modlist
 #Steamcmd install mods from modlist
 for m in `cat modlist`;
 do
-    ./steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/container +workshop_download_item 376030 $m +quit
+    /home/container/steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/container +workshop_download_item 376030 $m +quit
 done
+
+#If GameUserSettings doesn't exist, probably first run, exit
+if [ ! -f "/home/container/ShooterGame/Saved/Config/LinuxServer/GameUserSettings.ini" ];
+then
+  echo "First run, won't attempt to insert modlist until server rebooted"
+  exit 0
+fi
 
 #Set ActiveMods in ./ShooterGame/Saved/Config/LinuxServer/GameUserSettings.ini
 MODLISTLINE=$(cat modlist | grep "\S" | tr '\n' ',' | sed 's/.$//')
-if grep -qF "ActiveMods=" ShooterGame/Saved/Config/LinuxServer/GameUserSettings.ini;
+if grep -qF "ActiveMods=" /home/container/ShooterGame/Saved/Config/LinuxServer/GameUserSettings.ini;
 then
-    sed -i "s/ActiveMods=.*/ActiveMods=$MODLISTLINE/g" ShooterGame/Saved/Config/LinuxServer/GameUserSettings.ini
+    sed -i "s/ActiveMods=.*/ActiveMods=$MODLISTLINE/g" /home/container/ShooterGame/Saved/Config/LinuxServer/GameUserSettings.ini
 else
     #Insert ActiveMods=
-    sed -i "/[ServerSettings]/a ActiveMods=" ShooterGame/Saved/Config/LinuxServer/GameUserSettings.ini
-    sed -i "s/ActiveMods=.*/ActiveMods=$MODLISTLINE/g" ShooterGame/Saved/Config/LinuxServer/GameUserSettings.ini
+    sed -i "/[ServerSettings]/a ActiveMods=" /home/container/ShooterGame/Saved/Config/LinuxServer/GameUserSettings.ini
+    sed -i "s/ActiveMods=.*/ActiveMods=$MODLISTLINE/g" /home/container/ShooterGame/Saved/Config/LinuxServer/GameUserSettings.ini
 fi
