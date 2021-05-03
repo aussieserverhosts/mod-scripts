46876e5949c9da5d89a5d4e2bac823ec06b36fad
Michael <support@aussieserverhosts.com>
2021-05-03 21:08:48 +1000

    Fix to read variables

--- arkmods.sh
+++ arkmods.sh
@@ -25,7 +25,7 @@ sed -i '/^$/d' modlist
 #Steamcmd install mods from modlist
 for m in `cat modlist`;
 do
-    ./steamcmd.sh +login anonymous +force_install_dir /home/container +workshop_download_item 376030 $m +quit
+    ./steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/container +workshop_download_item 376030 $m +quit
 done
 
 #Set ActiveMods in ./ShooterGame/Saved/Config/LinuxServer/GameUserSettings.ini
