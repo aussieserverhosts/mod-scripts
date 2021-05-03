4a371ab84dac3875bbbe87e11fef9495466e3a4d
Michael <support@aussieserverhosts.com>
2021-05-03 20:17:56 +1000

    Initial arkmods script

---
+++ arkmods.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+MODID=$(echo $MODCOLLECTION | grep SubscribeCollectionItem | cut -d"'" -f2)
+
+echo > modlist
+
+#Strip mod IDs from Collection
+for i in $MODID
+do
+    echo $i | tee >> modlist
+done
+sed -i '/^$/d' modlist
+
+#Steamcmd install mods from modlist
+for m in `cat modlist`;
+do
+    ./steamcmd.sh +login anonymous +force_install_dir /mnt/server +workshop_download_item 376030 $m +quit
+done
+
+#Set ActiveMods in ./ShooterGame/Saved/Config/LinuxServer/GameUserSettings.ini
+MODLISTLINE=$(cat modlist | grep "\S" | tr '\n' ',' | sed 's/.$//')
+if grep -qF "ActiveMods=" ShooterGame/Saved/Config/LinuxServer/GameUserSettings.ini;
+then
+    sed -i "s/ActiveMods=.*/ActiveMods=$MODLISTLINE/g" ShooterGame/Saved/Config/LinuxServer/GameUserSettings.ini
+else
+    #Insert ActiveMods=
+    sed -i "/[ServerSettings]/a ActiveMods=" test.txt
+    sed -i "s/ActiveMods=.*/ActiveMods=$MODLISTLINE/g" ShooterGame/Saved/Config/LinuxServer/GameUserSettings.ini
+fi
